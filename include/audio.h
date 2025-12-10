f52742c024e5ef7b6c27571a86ff9e8d16993b9c
unknown <dvmvptd@gmail.com>
2025-12-10 21:37:16 +0700

    code music

--- include/audio.h
+++ include/audio.h
@@ -6,9 +6,16 @@
 
 void initAudio(void);
 void cleanupAudio(void);
-void playBackgroundMusic(void);
+void playBackgroundMusic(void); //nhac nen
 void stopBackgroundMusic(void);
-void playLossMusic(void);
-void playVictoryMusic(void);
+
+void playLossMusic(void); //nhac thua cuoc
+void playVictoryMusic(void); //nhac chien thang
+void playSuka(void); //nhac khi quai vat bi nguoi choi diet
+void playPlayerKilled(void); //nhac khi nguoi choi bi quai vat diet
+void playMenuSound(void); //nhac menu
+void playDrowningSound(void); //nhac khi het oxy
+void playInGame(void); //nhac khi dang choi
+
 
 #endif
